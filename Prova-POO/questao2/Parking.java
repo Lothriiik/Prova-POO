4cc6f6c4e2fb08317bb66b187ac58993ff7b1585
Uanderson Henrique <uanderson_pb@hotmail.com.br>
2022-10-19 20:17:53 -0300

    Update Parking.java

--- Prova-POO/questao2/Parking.java
+++ Prova-POO/questao2/Parking.java
@@ -12,26 +12,27 @@ public class Parking {
     Integer Total_Carros = 0;
     
     Map<String, Integer> estacionamento = new HashMap<>();
-
+//vai informar o carro e  quantas pessoa tem nele  e adicionar ao estacionamento.
     public String setCarros_Pessoas(String Carros2, Integer Pessoas2) {
         this.Carros = Carros2;
         this.Pessoas = Pessoas2;
         this.Total_Pessoas += Pessoas2;
         this.Total_Carros +=1;
+// adiciona ao estacionamento.
         setEstacionamento();
         return this.Carros;
 	}
-
+// vai informar o total de pessoas e carros no estaciomaneto.
     void getTotal_Pessoas(){
         System.out.println("Tem um total de " + Total_Pessoas + " pessoas no estacionamento e um total de " + Total_Carros + " carros no estacionamento.");
         
     }
-
+//adiciona carro ao estacionamento.
     void setEstacionamento(){
         estacionamento.put(Carros, Pessoas);
         
     }
-
+//informa o veiculo e quantas pessoas tem nele.
     void getEstacionamento(){
         for(String key : estacionamento.keySet()){
             System.out.println(key + ": " + estacionamento.get(key)+ " pessoas.");
