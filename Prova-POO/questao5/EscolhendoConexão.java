80c8bb507c09919a180754f8ad58186a34ba32a2
Uanderson Henrique <uanderson_pb@hotmail.com.br>
2022-10-19 20:25:49 -0300

    Update EscolhendoConexão.java

--- Prova-POO/questao5/EscolhendoConexão.java
+++ Prova-POO/questao5/EscolhendoConexão.java
@@ -3,20 +3,23 @@ package prova.questao5 ;
 class Escolhendo{
    
     String Escolha;
-   
+//iniciar a conexão
     void iniciarConexao(){
         System.out.println("Iniciando conexão...");
 
     }
+//finaliza a conexão
     void EncerrarConexao(){
         System.out.println("Encerrando conexão");
 
     }
+//começa o processamento da conexão
     void processarConexao(Conexao Conexao) {
         iniciarConexao();
         Conexao.escolheu(this);
         EncerrarConexao();
     }
+//vai definir o metodo de conexao dependendo da escolha.
     void EscolherConexao(String Escolha){
         if (Escolha == "Serial" ){
             processarConexao(new Serial());
