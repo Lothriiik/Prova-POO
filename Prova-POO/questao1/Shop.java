abb0c368e733b4a182571e452a3388b3d9f9b8d2
Uanderson Henrique <uanderson_pb@hotmail.com.br>
2022-10-19 20:11:22 -0300

    Update Shop.java

--- Prova-POO/questao1/Shop.java
+++ Prova-POO/questao1/Shop.java
@@ -8,18 +8,20 @@ public class Shop {
     private String Itens;
     private double Valor= 0;
     
-
+//adiciona um produto
     public String setProduto(String Produtos2) {
         this.Produtos=Produtos2;
+//coloca a compra no carrinho
         setCompras();
+//adiciona o valor do produto ao valor total
         setValor();
         return this.Produtos;
 	}
-
+//coloca a compra no carrinho
     void setCompras(){
         Compras.add(Produtos);
     }
-
+//vai dar o valor do produto com base no produto.
     private double setValor(){
         if (Produtos == "Arroz"){
             Valor += 4;
@@ -39,15 +41,16 @@ public class Shop {
         return this.Valor;
 
     }
-
+//vai informar os produtos
     void getValor_Produtos(){
         System.out.printf("Os Produtos Vendidos são :\n   Arroz    R$: 4,00\n   Carne    R$: 10,00\n   Feijao   R$: 5,00\n   Macarrao R$: 3,00\n   Sal      R$: 2,00\n\n");
         
     }
+//vai informar o valor total da compra
     void getValor_Compra(){
         System.out.printf("A compra deu um valor total de R$: %.2f%n", Valor);
     }
-
+//vai informar os produtos que estão no carrinho
     void getProdutos(){
         System.out.println("Os Produtos no carrinho  são: "+ Compras);
     }
